202639f89b1d4941e2fa65b55cc43f0805ba7b85
Hovo <voskanyanhovo@gmail.com>
2019-07-16 01:06:19 +0400

    fixed some bags but still undone

--- TicTacToe/js/scriptRec.js
+++ TicTacToe/js/scriptRec.js
@@ -1,353 +1,155 @@
-var board = GenerateStandartMatrix();
-var point = 0;
-//console.log(board);
-Math.
-var onClick= function (me)
-{
-    var info = me.id.split('_');
-    
-    if(board[info[1]-1][info[2]-1]!="X" && board[info[1]-1][info[2]-1]!="O" )
-    {
-        
-        board[info[1]-1][info[2]-1]="X";
-        //ongoing[info[1]-1][info[2]-1]="X";
-        
-    }
-   // console.log("the board",board);
-    var ongoing=GenerateStandartMatrix();
-   // console.log("start",ongoing);
-    for(var i=0;i<board.length;i++)
-    {
-        for(var j=0;j<board.length;j++)
-        {
-            if(board[i][j]=="X"||board[i][j]=="O")
-            {
-                ongoing[i][j]=board[i][j] ;
-                //console.log("vayhorsareev",ongoing);
+var board = getStandartMatrix();
 
-            }else{
-                ongoing[i][j]=4;
-                //console.log("vayhosadawedaareev",ongoing);
+
+
+var Play =
+{
+    length: 3,
+    width: 3,
+
+    draw: function (matrix)
+    {
+        for (var i = 1; i <= this.width; i++)
+        {
+
+            for (var j = 1; j <= this.length; j++)
+            {
+                var GetId = "i_" + i + "_" + j;
+                if (matrix[i-1][j-1] == "O")
+                {
+                    if (!document.getElementById(GetId).textContent)
+                    {
+                        document.getElementById(GetId).innerText = "O";
+                    }
+                }
+                if (matrix[i - 1][j - 1] == "X") {
+                    if (!document.getElementById(GetId).textContent) {
+                        document.getElementById(GetId).innerText = "X";
+                        // console.log(GetId);
+
+                    }
+                } else
+                {
+                    document.getElementById(GetId).innerText = "";
+                }
 
             }
         }
-    }
-    if(point==0)
-    {
-        board[1][1]=="X"?board[0][2]="O":board[1][1]="O";
-        point++;
-    }else
-    {
-        //console.log("ongoing",ongoing);
-       // console.log("board",board);
-       // console.log("This is ongoing matrix" );
-        var bestMove = FindBestMove(ongoing);
-       // console.log("Optimal move is  "+bestMove); 
-        board[bestMove[0]][bestMove[1]]="O";
-        point++;
-    }
-    DrawMatrix(board);
-}
+    },
 
 
-function FindBestMove(MyMatrix)
-{
-    var BestVal = -1000;
-    var results =[];
-   // console.log(MyMatrix);
-    for(var i=0; i<MyMatrix.length;i++)
+    getAllWinningCombinations: function (matrix)
     {
-        for(var j=0;j<MyMatrix.length;j++)
+        var Combs = [];
+        for (var i = 0; i < this.length; i++) {
+            Combs.push(matrix[i]);
+        }
+        for (var j = 0; j < this.length; j++) {
+            var Arr = [];
+            Arr.push(matrix[0][j]);
+            Arr.push(matrix[1][j]);
+            Arr.push(matrix[2][j]);
+            Combs.push(Arr);
+        }
+       var Arr = [];
+        for (var i = 0; i < this.length; i++)
         {
-            if(!isNaN(MyMatrix[i][j]))
+            for (var j = 0; j < this.width; j++)
             {
-                temp = MyMatrix[i][j];
-                MyMatrix[i][j]="O";
-                var moveValue=MiniMax(MyMatrix,0,false);
-               // console.log("this is move Value");
-               // console.log(moveValue);
-
-                MyMatrix[i][j]=temp;
-                if(moveValue>BestVal)
+                if (i == j)
                 {
-                    results=[];
-                    BestVal=moveValue;
-                    results.push(i);
-                    results.push(j);
-                   // console.log(results);
+                    Arr.push(matrix[i][j]);
+                }
+            }
+
+        }
+        Combs.push(Arr);
+        var Arr = [];
+
+        for (var i = 0; i < this.length; i++)
+        {
+            for (var j = 0; j < this.length; j++)
+            {
+                if (i == this.length - j - 1 || j == this.length - i - 1)
+                {
+                    Arr.push(matrix[i][j]);
                 }
             }
         }
+        Combs.push(Arr);
+        return Combs;
     }
-    console.log("the value of the best move is  "+BestVal);
-    return results;
 }
-
-
-function MiniMax(matrix1,step,myMove)
+function getStandartMatrix()
 {
-   
-    var score = Evaluate(matrix1);
-    console.log(matrix1,"This is matrix")
-    console.log(step,score);
-    //  if(step>3)
-    //  {
-    //      return 5;
-    //  }
-    if(score == 10)
+    var matrix = [];
+    var k = 0;
+    for (var i = 0; i <3; i++)
     {
-        return score;
-    }
-    if(score == -10)
-    {
-        return score;
-    }
-    if(score == 0)
-    {
-        return score;
-    }
-    //console.log(matrix1);
-   // return 
-    if(myMove)
-    {
-        var best = -1000;
-       // console.log(matrix1,"bababa");
-        for(var i=0;i<matrix1.length;i++)
+        matrix[i] = [];
+        for (var j = 0; j < 3; j++)
         {
-            for(var j=0;j<matrix1.length;j++)
-            {
-                if(!isNaN(matrix1[i][j]))
-                {
-                    var temp= matrix1[i][j];
-                    matrix1[i][j] = "O";
-                    return;
-                   best=Math.max(best,MiniMax(matrix1,step+1,!myMove));
-                   matrix1[i][j] =temp;
-                   // console.log(matrix1);
-                }
-            }
+            matrix[i][j] = k++;
         }
-        return best;
     }
-    else 
-    {
-        var best =1000;
-        for(var i=0;i<matrix1.length;i++)
-        {
-            for(var j=0;j<matrix1.length;j++)
-            {
-               var temp = matrix1[i][j];
-                matrix1[i][j]="X";
-                best = Math.min(best,MiniMax(matrix1,step+1,!myMove));
-                //console.log(matrix1);
-                matrix1[i][j]=temp;
-            }
-        }
-        return best;
-    }
-
+    return matrix;
 }
 
-function isMovesLeft(board) 
-{ 
-    for (var i = 0; i<3; i++) 
-    {
-        for (var j = 0; j<3; j++) 
-        {
-            if (!isNaN(board[i][j])) 
-            {
-                return true; 
-            }
-        }
-    }
-    return false; 
-} 
-function Evaluate(matrixprop)
+function CheckMatrix( Arrays )
 {
-    if(MatrixCheck(matrixprop)==0)
+    for (var i = 0; i < Arrays.length; i++)
+    {
+        if (CheckArray(Arrays[i]))
         {
-           
-            return 10;
-        }else if(MatrixCheck(matrixprop)==1)
+            alert(Arrays[i][0] + " Has Won");
+            if (isNaN(Arrays[i][0])) { 
+                board = getStandartMatrix();
+                return Arrays[i][0] == "X" ? 1 : 0;
+            }
+        }
+        if (IsMovesLeft(board))
         {
-            
-            return -10;
-   
-        }else if(MatrixCheck(matrixprop)==-1)
-        {
-           
-           return 0;
-        }     
-}
+            board = getStandartMatrix();
+            return -1;
+        }
 
-function GenerateStandartMatrix()
+    }
+}
+function IsMovesLeft(matrix)
 {
-   var k =0;
-    var board=[];
-   for(var i =0;i<3;i++)
-   {
-       board[i]=[];
-       for(var j =0;j<3;j++)
-       {
-           
-           board[i][j]=k;
-           k++;
-       }
-   }
-  // console.log(board);
-   return board;
-}
-
-function DrawMatrix(matrix)
- {
-    for(var i =1;i<=3;i++)
+    for (var i = 0; i < matrix.length; i++)
     {
-        for(var j =1;j<=3;j++)
+        for (var j = 0; j < matrix[i].length; j++)
         {
-            var getID ="i_"+i+"_"+j;
-            if(matrix[i-1][j-1]=="O")
-            {
-                if(!document.getElementById(getID).textContent)
-                {
-                    document.getElementById(getID).innerText="O";
-                    setTimeout(function(){},0);
-                }
-            }
-            if(matrix[i-1][j-1]=="X")
-            {
-                if(!document.getElementById(getID).textContent)
-                {
-                    document.getElementById(getID).innerText="X";
-                    //console.log(matrix);
-                    setTimeout(function(){},0);
-                }
-                
-            }
-            if(matrix[i-1][j-1]!="X" && matrix[i-1][j-1]!="O")
-            {
-                document.getElementById(getID).innerText="";
-                setTimeout(function(){},0);
-            }
+            if (isNaN(matrix[i][j])) return false;
         }
     }
- }
+    return true;
+}
 
-
- function getAllWinningCombinations(matrix2)
- {
-    var Combs =[];
-    var colums =[];
-   // Combs.push("toxer");
-
-    for(var i=0;i<matrix2.length;i++)
-    {
-        Combs.push(matrix2[i]);
-    }
-  //  Combs.push("syuner");
-    for(var i =0; i<matrix2.length;i++)
-    {
-        colums=[];
-       colums.push(matrix2[0][i]);
-       colums.push(matrix2[1][i]);
-       colums.push(matrix2[2][i]);
-
-      // console.log("etzibily");
-        //console.log(colums);
-
-        Combs.push(colums);
-
-    }
-   // Combs.push("ankyunagcer");
-    //console.log(GetDiagonal(3,0));
-    //console.log(GetDiagonal(0,3));
-    Combs.push(GetDiagonal(3,0,matrix2),GetDiagonal(0,3,matrix2));
-    
-    return Combs;
- }
- function CheckArray(array)
- {
-     var Istrue=true;
-     for(var i = 0;i<array.length;i++)
-     {
-        // console.log("asdad");
-         if(array[0]!=array[i])
-         {
-            // console.log(array[0]+array[i]);
-            Istrue=false;
-         }
-     }
-     return Istrue;
- }
-
- function GetDiagonal(start,end,matrix)
+function CheckArray(Arr)
 {
-    var Arr=[];
-    if(start<end)
+    for (var i = 0; i < Arr.length; i++)
     {
-        for( var x=start;x<end;x++)
-        {
-            Arr.push(matrix[x][x]);
-        }
-       
-    }else
-    {
-        for(var x=0;x<matrix.length;x++)
-        {
-            Arr.push(matrix[start-1-x][x]);
-        }
-        
+        if (Arr[0] != Arr[i]) return false;
     }
-    return Arr;
-
+    return true;
 }
+var onClick = function (me)
+{
+    var info = me.id.split("_");
 
-function XorO (Arr)
- {
-    if(Arr[0]=='O')
-    {
-        
-        return 0;
-    }else if(Arr[0]=='X')
-    {
-        return 1;
+    if (board[info[1] - 1][info[2] - 1] != "O" && board[info[1] - 1][info[2] - 1] !="X") board[info[1] - 1][info[2] - 1] = "X";
+
+    var winCombs = Play.getAllWinningCombinations(board);
+
+    console.log(board);
+   // console.log(winCombs);
+
+    Play.draw(board);
+    setTimeout(CheckMatrix(winCombs), 10);
+
+    if (!isNaN(CheckMatrix(winCombs))) {
+        board = getStandartMatrix();
     }
- }
-
- function CheckArray(array)
- {
-     var Istrue=true;
-     for(var i = 0;i<array.length;i++)
-     {
-        // console.log("asdad");
-         if(array[0]!=array[i])
-         {
-            // console.log(array[0]+array[i]);
-            Istrue=false;
-         }
-     }
-     return Istrue;
- }
-
- function MatrixCheck(matrix)
- {
-    //var Arr = [];
-    
-    for(var i=0;i<getAllWinningCombinations(matrix).length;i++)
-    {
-        if(CheckArray(getAllWinningCombinations(matrix)[i]))
-        {
-            return XorO(getAllWinningCombinations(matrix)[i]);
-        }
-    }
-       // Arr=[];
-
-        var draw =true;
-        for(var i=0;i<matrix.length;i++)
-        {
-            for(var j=0;j<matrix.length;j++)
-            {
-                if(!isNaN(matrix[i][j])) draw =false;
-            }
-        }
-        if(draw) return -1;
-    }
+}
